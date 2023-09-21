1c5425ee778c5a2575138a04806ae7b7f331e505
varaprasadrdy <139739073+varaprasadrdy@users.noreply.github.com>
2023-09-21 10:04:13 +0530

    Add files via upload

---
+++ SsdBlogApp/src/main/java/com/ssd/blog/app/SsdBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.ssd.blog.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsdBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsdBlogAppApplication.class, args);
+	}
+
+}
---
+++ SsdBlogApp/src/test/java/com/ssd/blog/app/SsdBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssd.blog.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsdBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
